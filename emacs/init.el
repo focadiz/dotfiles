21582c0f0c7f97d4407908514afe0963d5c198d9
Felipe Ocadiz <felipe@ocadiz.net>
2020-07-29 17:59:34 -0700

    Emacs: init.el added

---
+++ emacs/init.el
@@ -0,0 +1,6 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
