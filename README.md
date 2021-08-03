eb281dcd1191891de40a294e441a2c0c74916ad7
Felipe Ocadiz <felipe@ocadiz.net>
2021-08-02 23:28:19 -0700

    Moving load theme function from init.el. Creating i3 config folder

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ ln -s [path to repo]/emacs/init.el ~/.emacs.d/init.el
 To install packages:
 M-x package-install cider
 M-x package-install dracula-theme
+
+## i3
+i3/config -> ~/.config/i3/config
+
+## urxvt
+
+## neofetch
+
+## zsh
--- emacs/init.el
+++ emacs/init.el
@@ -2,7 +2,6 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-(load-theme 'dracula t)
 (custom-set-variables '(initial-frame-alist (quote ((fullscreen . maximized)))))
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
