cb128d87141874421ef9f293ecc45dfd13a9df60
Felipe Ocadiz <felipe@ocadiz.net>
2021-08-14 00:40:27 -0700

    AUR Packages added, typo fixed

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 export PWD=$(pwd) 
 ln -s $PWD/i3/xinitrc ~/.xinitrc
-ln -s $PWD/i3/config ~/.confing/i3/config
+ln -s $PWD/i3/config ~/.config/i3/config
 ln -s $PWD/urxvt/Xresources ~/.Xresources
 ln -s $PWD/zsh/zshrc ~/.zshrc
 ln -s $PWD/zsh/p10k.zsh ~/.p10k.zsh
