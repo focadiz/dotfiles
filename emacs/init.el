78d4d56ca212f3053968b79c7f6c934022927fd5
Felipe Ocadiz <felipe@ocadiz.net>
2024-01-20 18:46:46 -0800

    Loading dracula theme

--- emacs/init.el
+++ emacs/init.el
@@ -12,10 +12,12 @@
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
-(set-frame-font "Hack Nerd Font Mono 10" nil t)
+(set-frame-font "Hack Nerd Font Mono 12" nil t)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(load-theme 'dracula t)
